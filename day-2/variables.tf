e7d32df065d0ee20772400ec4993605e685dceac
venkat <venkat.infadm@gmail.com>
2025-02-25 10:47:46 +0530

    added

---
+++ day-2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+ description = "Inserting ami id value" 
+ type = string
+ default = ""
+
+}
+
+variable "type" {
+  type = string
+  default = ""
+}
+
+variable "key" {
+  type = string
+  default = ""
+}
